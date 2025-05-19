813d82f6ada44baf01f6e136cfc710d5b4ad2195
Ekramy Latief <ekramyl3000@gmail.com>
2025-05-19 15:55:07 +0300

    add first migration and update database

--- Skinet.Core/Entities/Product.cs
+++ Skinet.Core/Entities/Product.cs
@@ -1,9 +1,14 @@
-﻿namespace Skinet.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skinet.Core.Entities;
 
 public class Product:BaseEntity
 {
     public required string Name { get; set; }
     public required string Description { get; set; }
+    
+    [Precision(18,2)]
     public required decimal Price { get; set; }
     public required string PictureUrl { get; set; }
     public required string Type { get; set; }
---
+++ Skinet.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet.Core.Entities;
+
+namespace Skinet.Infrastructure.Data;
+
+public class StoreContext(DbContextOptions options): DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    
+}
